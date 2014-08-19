8ba80b1a42c7906504f11675428ea834e633d6d1
Theodor Văraru <theo@vararu.org>
2014-08-19 12:35:56 +0300

    Initial fancy fullscreen transition thing.

--- app/js/pages/media.js
+++ app/js/pages/media.js
@@ -12,24 +12,28 @@ module.exports = View.extend({
   render: function () {
     this.$el.html(this.template());
 
-    var speed = 300, easing = mina.backout;
+    setTimeout(function () {
+      new window.grid3D( document.getElementById( 'grid3d' ) );
+    }, 500);
 
-    // Attach fancy ShapeHoverEffect listeners.
-    this.$('.grid > a').each(function (index, el) {
-      var s = Snap( el.querySelector('svg') ), path = s.select('path'),
-        pathConfig = {
-          from : path.attr('d'),
-          to : el.getAttribute('data-path-hover')
-        };
-
-      el.addEventListener('mouseenter', function() {
-        path.animate( { 'path' : pathConfig.to }, speed, easing );
-      } );
-
-      el.addEventListener( 'mouseleave', function() {
-        path.animate( { 'path' : pathConfig.from }, speed, easing );
-      } );
-    });
+    // var speed = 300, easing = mina.backout;
+    //
+    // // Attach fancy ShapeHoverEffect listeners.
+    // this.$('.grid > a').each(function (index, el) {
+    //   var s = Snap( el.querySelector('svg') ), path = s.select('path'),
+    //     pathConfig = {
+    //       from : path.attr('d'),
+    //       to : el.getAttribute('data-path-hover')
+    //     };
+    //
+    //   el.addEventListener('mouseenter', function() {
+    //     path.animate( { 'path' : pathConfig.to }, speed, easing );
+    //   } );
+    //
+    //   el.addEventListener( 'mouseleave', function() {
+    //     path.animate( { 'path' : pathConfig.from }, speed, easing );
+    //   } );
+    // });
 
     return this;
   }
